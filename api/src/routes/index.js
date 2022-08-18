bdecf9f522a04d918fec5da3eb555fd820110643
Santi O <s.ostrovsky@hotmail.com>
2022-08-18 09:50:01 -0300

    Routes-update

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,7 +4,12 @@ const {
   findCountries,
   getCountryById,
 } = require("../controllers/Country");
-const activities = require("../controllers/Activity");
+const {
+  getActivities,
+  addActivity,
+  updateActivity,
+  deleteActivity,
+} = require("../controllers/Activity");
 const { Country } = require("../db");
 
 // Importar todos los routers;
@@ -17,26 +22,98 @@ const router = Router();
 // router.use("/countries", countries);
 // router.use("/activities", activities);
 
-// GET --> /COUNTRIES
+// --------------
+// COUNTRY ROUTES
+// --------------
+
+// GET @ /COUNTRIES + /COUNTRIES ? NAME (QUERY)
 router.get("/countries", async (req, res) => {
   try {
-    // determine if database is empty
-    const cont = await Country.count();
-    // if database is empty, execute getCountries
-    if (!cont) await getCountries();
-    // once database has all data, execute findCountries
-    const find = await findCountries();
-    find.length ? res.json(find) : res.send("ERROR");
+    let { name } = req.query;
+    if (name)
+      name = name
+        .trim()
+        .split(" ")
+        .map((e) => e[0].toUpperCase() + e.slice(1).toLowerCase())
+        .join(" ");
+    // verify if DB is empty
+    const count = await Country.count();
+    // if DB is empty, execute getCountries
+    if (!count) await getCountries();
+    // once the DB has all API data, execute findCountries
+    const find = await findCountries(name);
+    find.length
+      ? res.json(find)
+      : res.status(400).send("Error @ routes/index/countries");
     // res.send(await findCountries());
-  } catch (error) {
-    console.log(error.message);
+  } catch (e) {
+    // console.error(e.message);
+    res.status(404).send(`Error --→ ${e}`);
   }
 });
 
-// GET /COUNTRIES /ID (PARAMS)
-router.get("/countries/:id", async (req, res) => {});
+// GET @ /COUNTRIES /ID (PARAMS)
+router.get("/countries/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    /*
+    // verify DB  is empty
+    const count = await Country.count();
+    // if DB is empty, execute getCountries
+    if (!count) await getCountries();
+    */
+    // once the DB has all API data, execute getCountryById
+    res.json(await getCountryById(id.toUpperCase()));
+  } catch (e) {
+    // console.error(e.message)
+    res.status(404).send(`Error --→ ${e}`);
+  }
+});
 
-// GET / COUNTRIES /NAME (QUERY)
-router.get("/countries", async (req, res) => {});
+// ----------------
+// ACTIVITY ROUTES
+// ----------------
+
+// GET @ /ACTIVITIES
+router.get("/activities", async (req, res) => {
+  try {
+    const find = await getActivities();
+    find.length ? res.json(find) : res.send(`No activities were created yet!`);
+  } catch (e) {
+    res.status(404).send(`Error --> ${e}`); // Not found
+  }
+});
+
+// POST @ /ACTIVITIES
+router.post("/activities", async (req, res) => {
+  try {
+    // const { name, difficulty, duration, season } = req.body;
+    const newActivity = await addActivity(req.body);
+    res.send(newActivity);
+  } catch (e) {
+    res.status(400).send(`Error --> ${e}`); // Bad request
+  }
+});
+
+// PUT @ /ACTIVITIES /ID (PARAMS)
+router.put("/activities/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedActivity = await updateActivity(id, req.body);
+    res.send(updatedActivity);
+  } catch (e) {
+    res.status(404).send(`Error --> ${e}`); // Not found
+  }
+});
+
+// DELETE @ /ACTIVITIES /ID (PARAMS)
+router.delete("/activities/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await deleteActivity(id);
+  } catch (e) {
+    res.status(400).send(`Error --> ${e}`); // Bad request
+  }
+});
 
 module.exports = router;
