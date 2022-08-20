b9277750616cb0d11869116c6173d142b98d7b72
Santi O <s.ostrovsky@hotmail.com>
2022-08-20 12:21:04 -0300

    controllers-update

--- api/src/controllers/Activity.js
+++ api/src/controllers/Activity.js
@@ -4,9 +4,13 @@ const { Country, Activity } = require("../db");
 const getActivities = async () => {
   try {
     // Bring all activities from the DB (including countries)
-    const find = await Activity.findAll({ include: [Country] });
+    const find = await Activity.findAll(
+      { order: ["id"] },
+      { include: [Country] }
+    );
     //
     console.log(`getActivities was executed successfully`);
+    console.log(find);
     return find;
   } catch (e) {
     // Error msg in case data call failed
@@ -17,14 +21,23 @@ const getActivities = async () => {
 // Post --> addActivity
 const addActivity = async (content) => {
   try {
-    const { name, difficulty, duration, season } = content;
+    const { name, difficulty, duration, season, countries } = content;
+    if (!name || !difficulty || !duration || !season || !countries) {
+      console.error("Missing Info");
+      return;
+    }
+    // Create new activity
     const newActivity = await Activity.create({
       name,
       difficulty,
       duration,
       season,
+      countries,
     });
-    console.log(`Activity successfully added to database:\n${newActivity}`);
+    //   Link activity to its countries
+    await newActivity.addCountry(countries);
+
+    console.log(`Activity successfully added to database`);
     return newActivity;
   } catch (e) {
     // Error msg in case data insertion failed
@@ -35,13 +48,15 @@ const addActivity = async (content) => {
 // PUT --> updateActivity
 const updateActivity = async (id, content) => {
   try {
-    const { name, difficulty, duration, season } = content;
+    const { name, difficulty, duration, season, countries } = content;
     const activity = await Activity.findByPk(id);
     // update fields gotten by content
     if (name) activity.name = name;
     if (difficulty) activity.difficulty = difficulty;
     if (duration) activity.duration = duration;
     if (season) activity.season = season;
+    //
+    // PUT ACTIVITIES LINK TO COUNTRIES
     // save changes on row and return updated activity
     await activity.save();
     return activity;
