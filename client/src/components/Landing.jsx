028b483cce6f0b2c7f65b84437efcddd478af602
Santi O <s.ostrovsky@hotmail.com>
2022-08-20 12:24:16 -0300

    landing-update

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/Landing.css";
 
 export default function Landing() {
   return (
-    <div>
+    <div className="container">
       <h1 className="App">Santi's Countries</h1>
+      <Link to="/countries">
+        <button autoFocus>DISCOVER</button>
+      </Link>
     </div>
   );
 }
